52bbfd6639cef76d9d718ff37740b3957512e981
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-10-13 09:39:26 -0300

    feat: added firebase configs

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,20 @@
 import app from "./app"
 
+import {initializeApp} from "firebase/app"
+import * as admin from "firebase-admin"
+
+admin.initializeApp()
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC9FsYu5RxXsEc5ZxfDsN2ZJ14V3gctAMI",
+    authDomain: "to-do-list-c831c.firebaseapp.com",
+    projectId: "to-do-list-c831c",
+    storageBucket: "to-do-list-c831c.appspot.com",
+    messagingSenderId: "523431767352",
+    appId: "1:523431767352:web:eeef0a96447283d60de5cd",
+    measurementId: "G-VK18MYP21C"
+}
+
+export const firebaseApp = initializeApp(firebaseConfig)
+
 app.listen(3030, (): void => console.log("Server running on port 3030"))
