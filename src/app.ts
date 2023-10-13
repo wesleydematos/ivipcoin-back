a15c5128b168c781d73c1d9b4319cd64f3c9879b
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-10-13 09:17:42 -0300

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import express, {Application} from "express" 
+import cors from "cors"
+
+const app: Application = express() 
+
+app.use(express.json())
+app.use(cors())
+
+export default app
---
+++ src/server.ts
@@ -0,0 +1,3 @@
+import app from "./app"
+
+app.listen(3030, (): void => console.log("Server running on port 3030"))
