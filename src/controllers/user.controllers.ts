0c2d72e859d99c45dfb7b3cead5104883a2bd431
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-10-14 12:37:14 -0300

    feat: created user service added

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,12 +1,22 @@
 import {iUserRegister} from "../interfaces"
+import { registerSchema } from "../schemas";
 import {registerUserService} from "../services"
 
 import {Request, Response} from "express"
+import { ValidationError } from 'yup'
 
 const registerUserController = async (req: Request, res: Response) => {
-    const userData: iUserRegister = req.body;
-    const newUser = await registerUserService(userData);
-    return res.status(201).json(newUser);
+    const userData: iUserRegister = req.body
+
+    try {
+        const validatedRegister = await registerSchema.validate(userData, {
+          abortEarly: false,
+        })
+        const [newUser, status] = await registerUserService(validatedRegister)
+        return res.status(status as number).json(newUser)
+      } catch (error: ValidationError | any) {
+        return res.status(400).json({ message: error.errors || error})
+      }
 }
 
 export {registerUserController}
--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -1,8 +1,31 @@
-import {iUserRegister} from "../interfaces"
+import {iTask, iUserRegister} from "../interfaces"
+import {db} from "../server"
+
+async function readData(collectionName:string) {
+    const snapshot = await db.collection(collectionName).get()
+
+    const data: any = []
+    snapshot.forEach((doc)=>{
+        data.push({id: doc.id, ...doc.data()})
+    })
+
+    return data
+}
+
+async function createData(collectionName: string, data: iUserRegister | iTask) {
+    const docRef = db.collection(collectionName).doc()
+    await docRef.set(data)
+
+    return docRef
+}
 
 const registerUserService = async (userData: iUserRegister) => {
-    console.log(userData)
-    return userData
+    try {
+        await createData("users", userData)
+        return [{message: "Usuário criado!"}, 201]
+    } catch (error) {
+        return [{message: error}, 400]
+    }
 }
 
 export {registerUserService}
