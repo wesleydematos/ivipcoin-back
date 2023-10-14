b9f097a910fd94f5c7f4a5d8107c26a3514d5d05
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-10-14 16:13:45 -0300

    feat: added environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,22 @@
-import app from "./app"
-var serviceAccount = require("../serviceAccountKey.json") 
+import "dotenv/config"  
 
-import {initializeApp} from "firebase/app"
+import app from "./app"
+
+import { initializeApp } from "firebase/app"
 import * as admin from "firebase-admin"
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-})
+admin.initializeApp()
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC9FsYu5RxXsEc5ZxfDsN2ZJ14V3gctAMI",
-    authDomain: "to-do-list-c831c.firebaseapp.com",
-    projectId: "to-do-list-c831c",
-    storageBucket: "to-do-list-c831c.appspot.com",
-    messagingSenderId: "523431767352",
-    appId: "1:523431767352:web:eeef0a96447283d60de5cd",
-    measurementId: "G-VK18MYP21C"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
 }
 
-export const db = admin.firestore()
 export const firebaseApp = initializeApp(firebaseConfig)
 
 app.listen(3030, (): void => console.log("Server running on port 3030"))
